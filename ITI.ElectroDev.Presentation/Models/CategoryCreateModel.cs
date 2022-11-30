4e033fcd9d82587439a916dfc035128fe17a12d5
mariamayman62 <mariamayman6298@gmail.com>
2022-11-30 22:55:11 +0200

    Modify in Category Controller

--- ITI.ElectroDev.Presentation/Models/CategoryCreateModel.cs
+++ ITI.ElectroDev.Presentation/Models/CategoryCreateModel.cs
@@ -6,7 +6,7 @@ namespace ITI.ElectroDev.Presentation.Models
     public class CategoryCreateModel
     {
         public int Id { get; set; }
-        [Display(Name = "Title")]
+        [Display(Name = "Category Name")]
         [Required(ErrorMessage = "حقل العنوان مطلوب")]
         [MaxLength(200, ErrorMessage = "لا يزيد عن 200")]
         [MinLength(4, ErrorMessage = "Must Be More Than Or Equals 4 Chars.")]
