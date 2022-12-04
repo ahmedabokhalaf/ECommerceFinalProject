a5d0ee1d7df49cc0f650ce119ae6a449f9ab44e7
mariamayman62 <mariamayman6298@gmail.com>
2022-12-04 16:49:34 +0200

    add localization in orders views

--- ITI.ElectroDev.Presentation/Models/OrderViewModel.cs
+++ ITI.ElectroDev.Presentation/Models/OrderViewModel.cs
@@ -7,16 +7,16 @@ namespace ITI.ElectroDev.Presentation
     {
         public int Id { get; set; }
         [Required(ErrorMessage ="You must select customer")]
-        [Display(Name ="Order For User: ")]
+        [Display(Name ="Order For User:")]
         public string UserId { get; set; }
         [Required(ErrorMessage = "You must select product")]
-        [Display(Name = "Product: ")]
+        [Display(Name = "Product:")]
         public int ProductId { get; set; }
-        [Display(Name = "Total Price: ")]
+        [Display(Name = "Total Price:")]
         public double TotalPrice { get; set; }
-        [Display(Name ="Type Of Order: ")]
+        [Display(Name ="Type Of Order:")]
         public string Type { get; set; }
-        [Display(Name = "Status Of Order: ")]
+        [Display(Name = "Status Of Order:")]
         public string Status { get; set; }
         [Display(Name = "Date:")]
         public DateTime CreatedAt { get; set; }
